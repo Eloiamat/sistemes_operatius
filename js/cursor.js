fb29f0c3f519132115727cf96d984ac8625c33dc
Eloiamat <eamat@iesebre.com>
2026-09-16 00:57:08 +0200

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -6,13 +6,12 @@ const cursor = document.querySelector(".cursor-personalitzat");
 
 if (cursor) {
 
-    cursor.style.left = window.innerWidth / 2 + "px";
-    cursor.style.top = window.innerHeight / 2 + "px";
-
     document.addEventListener("mousemove", (e) => {
 
         cursor.style.left = e.clientX + "px";
         cursor.style.top = e.clientY + "px";
+
+        document.body.classList.add("cursor-actiu");
 
     });
 
